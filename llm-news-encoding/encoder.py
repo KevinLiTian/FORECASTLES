12a1f9534d1946450dc837ca3d4e40dfa0303c8f
Abhijoy-Mandal <a.mandal@mail.utoronto.ca>
2023-10-26 19:42:34 -0400

    encoder completed

--- llm-news-encoding/encoder.py
+++ llm-news-encoding/encoder.py
@@ -1,45 +1,35 @@
 import torch
-from pytorch_transformers import BertTokenizer, BertModel, BertForMaskedLM
+# from pytorch_transformers import BertTokenizer, BertModel, BertForMaskedLM
+from transformers import AutoTokenizer, BertModel, OpenAIGPTModel
 
 # OPTIONAL: if you want to have more information on what's happening under the hood, activate the logger as follows
 import logging
 logging.basicConfig(level=logging.INFO)
-if __name__ == "__main__":
-    # Load pre-trained model tokenizer (vocabulary)
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
+
+def encode_text(text, max_len=50):
+    """
+    Given a list of text, tokenize it and encode it
+    :param max_len: Max length of string
+    :param text: list[str]
+    :return: tensor of hidden state encodings (N x M x 768) and the attention masks (N x M)
+            where M is the length of the largest string.
+    """
+    text.append('A '*max_len)
+    tokenizer = AutoTokenizer.from_pretrained("openai-gpt")
+    tokenizer.pad_token = '[PAD]'
+    tokenizer.padding_side = "left"
     # Tokenize input
-    text = "[CLS] Who was Jim Henson ? [SEP] Jim Henson was a puppeteer [SEP]"
-    tokenized_text = tokenizer.tokenize(text)
-
-    indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
-    # Define sentence A and B indices associated to 1st and 2nd sentences (see paper)
-    segments_ids = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
-
-    # Convert inputs to PyTorch tensors
-    tokens_tensor = torch.tensor([indexed_tokens])
-    segments_tensors = torch.tensor([segments_ids])
-
+    tokenized_text = tokenizer(text, return_tensors='pt', padding=True)
     # Load pre-trained model (weights)
-    model = BertModel.from_pretrained('bert-base-uncased')
+    model = OpenAIGPTModel.from_pretrained("openai-gpt")
+    outputs = model(input_ids=tokenized_text['input_ids'],
+                    attention_mask=tokenized_text['attention_mask'],
+                    # pad_token_id=tokenizer.pad_token_id,
+                    )
+    last_hidden_states = outputs.last_hidden_state
+    return last_hidden_states[:-1], tokenized_text['attention_mask'][:-1]
 
-    # Set the model in evaluation mode to desactivate the DropOut modules
-    # This is IMPORTANT to have reproductible results during evaluation!
-    model.eval()
 
-    # If you have a GPU, put everything on cuda
-    # tokens_tensor = tokens_tensor.to('cuda')
-    # segments_tensors = segments_tensors.to('cuda')
-    # model.to('cuda')
-
-    # Predict hidden states features for each layer
-    with torch.no_grad():
-        # See the models docstrings for the detail of the inputs
-        outputs = model(tokens_tensor, token_type_ids=segments_tensors)
-        # PyTorch-Transformers models always output tuples.
-        # See the models docstrings for the detail of all the outputs
-        # In our case, the first element is the hidden state of the last layer of the Bert model
-        encoded_layers = outputs[0]
-        print(encoded_layers.shape)
-    # We have encoded our input sequence in a FloatTensor of shape (batch size, sequence length, model hidden dimension)
-    assert tuple(encoded_layers.shape) == (1, len(indexed_tokens), model.config.hidden_size)
+if __name__ == "__main__":
+    last_hidden_states, mask = encode_text(["Who what Jim? Jim was a clown.", "Hello World"])
