c28e74a8048c2be2a821304f8220346804c2a045
Kimlin Chin <chinkam123@gmail.com>
2023-12-08 09:46:18 -0500

    Move files

--- location-metadata/01-download_data.R
+++ location-metadata/01-download_data.R
@@ -5,8 +5,9 @@
 #### Workspace setup ####
 library(opendatatoronto)
 library(dplyr)
+library(sf)
 
-#### Download data ####
+#### Download Neighbourhood Profiles data ####
 # get package
 package <- show_package("6e19a90f-971c-46b3-852c-0c48c436d1fc")
 
@@ -17,7 +18,18 @@ resources <- list_package_resources("6e19a90f-971c-46b3-852c-0c48c436d1fc")
 data <- filter(resources, name == 'neighbourhood-profiles-2021-158-model') %>% get_resource()
 
 # get census neighbourhood profile data and change to dataframe
-df <- do.call(rbind.data.frame, data["hd2021_census_profile"])
+neighbourhood_profiles <- do.call(rbind.data.frame, data["hd2021_census_profile"])
+
+#### Download Neighbourhood GEOJSON data ####
+# get package
+package <- show_package("neighbourhoods")
+
+# get all resources for this package
+resources <- list_package_resources("neighbourhoods")
+
+# load the Neighbourhoods geojson
+neighbourhood_geojson <- filter(resources, name == 'Neighbourhoods - 4326.geojson') %>% get_resource()
 
 #### Save data ####
-write_csv(df, file='neighbourhood-profiles-2021-158-model.csv')
+write_csv(neighbourhood_profiles, file='neighbourhood-profiles-2021-158-model.csv')
+st_write(neighbourhood_geojson, dsn = 'Neighbourhoods - 4326.geojson')
